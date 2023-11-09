2b090209fe29994a048f4445eec702c87fa4793b
moose <1728093819@qq.com>
2023-11-09 14:52:39 +0000

    update ch5_3 with ipc methods

--- ch5/ch5_3.c
+++ ch5/ch5_3.c
@@ -4,9 +4,25 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
+typedef enum{
+    IPC_METHOD_MSG_PIPE,
+    IPC_METHOD_MSG_FIFO,
+    IPC_METHOD_MSG_SOCKET,
+    IPC_METHOD_MSG_SYS_V_MQ,
+    IPC_METHOD_MSG_POSIX_MQ,
+    IPC_METHOD_MSG_SIGNAL,
+    IPC_METHOD_SHM_SYS_V,
+    IPC_METHOD_SHM_POSIX,
+    IPC_METHOD_SHM_MMAP,
+} IPC_METHOD_T;
+
 int 
-wait_child_process()
+wait_child_process(IPC_METHOD_T ipc_method)
 {
+    switch(ipc_method)
+    {
+        
+    }
     
 }
 
